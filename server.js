96984e164671857a5550001b87b06a7573e95a17
Danial Alinasiri <your@email.com>
2026-06-07 12:53:06 +0430

    corecting server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const app = express();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase URL and Key are required. Please set SUPABASE_URL and SUPABASE_KEY environment variables.");
