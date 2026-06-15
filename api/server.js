c8c7bd8afcccf5f128cc1411e13fc227e274228a
Danial Alinasiri <your@email.com>
2026-06-15 19:54:52 +0430

    ?

--- api/server.js
+++ api/server.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY;
 
